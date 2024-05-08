a0172323d3fc825380c26dc23f201b21e90f18da
Muhammad Arfath <146614939+muhammedarfath@users.noreply.github.com>
2024-05-08 10:35:38 +0530

    backend success

--- chat/views.py
+++ chat/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
-
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request, "chat/index.html")
 
 
 def room(request, room_name):
-    return render(request, "chat/room.html", {"room_name": room_name})
+    return render(request, "chat/room.html", {"room_name": room_name,"username":request.user.username})
