50eb9463866916a56fccd6130c77c79ddf1515f4
Muhammad Arfath <146614939+muhammedarfath@users.noreply.github.com>
2024-05-12 11:18:19 +0530

    edit models

--- chat/admin.py
+++ chat/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 
-from .models import Profile,User
+from .models import Profile,User,Message
 
 # Register your models here.
+
+class ChatMessageAdmin(admin.ModelAdmin):
+    list_editable = ['is_read']
+    list_display = ['sender','receiver','content','is_read']
+
 admin.site.register(Profile)
 admin.site.register(User)
+admin.site.register(Message,ChatMessageAdmin)
--- chat/models.py
+++ chat/models.py
@@ -27,23 +27,38 @@ class Profile(models.Model):
     
 
 
-
-class Message(models.Model):
-    author = models.ForeignKey(User,related_name='author_messages',on_delete=models.CASCADE)
-    content = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self):
-        return self.author.username
-    
-    def last_10_messages():
-        return Message.objects.order_by('timestamp').all()[:100]
-    
-    
-    
 def created_user_profile(sender,instance,created,**kwargs):
     if created:
         Profile.objects.create(user=instance)
     
     
-post_save.connect(created_user_profile,sender=User)      
+post_save.connect(created_user_profile,sender=User)    
+
+
+class Message(models.Model):
+    author = models.ForeignKey(User,related_name='user',on_delete=models.CASCADE)
+    sender = models.ForeignKey(User,related_name='sender',on_delete=models.CASCADE,blank=True, null=True)
+    receiver = models.ForeignKey(User,related_name='receiver',on_delete=models.CASCADE,blank=True, null=True)
+    content = models.TextField()
+    is_read = models.BooleanField(default=False,blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['timestamp']
+        verbose_name_plural = 'Message'
+    
+    def __str__(self):
+        return f"{self.sender} - {self.receiver}"
+    
+    @property
+    def sender_profile(self):
+        sender_profile = Profile.objects.get(user=self.sender)
+        return sender_profile
+    
+    @property
+    def receiver_profile(self):
+        receiver_profile = Profile.objects.get(user=self.receiver)
+        return receiver_profile
+    
+    
+  
